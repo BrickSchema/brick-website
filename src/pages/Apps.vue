4a4c1d3e3967760cd0c5b5335138d751e489485b
Shreyas Nagare <shreyasnagare@gmail.com>
2019-07-05 15:41:00 -0400

    Create Apps page

---
+++ src/pages/Apps.vue
@@ -0,0 +1,55 @@
+<template>
+    <Layout>
+        <div class="container-inner mx-auto py-16">
+            <div class="apps flex">
+                <div>
+                    <categories class="list"/>
+
+                </div>
+                <apps class="flex-grow"/>
+            </div>
+        </div>
+    </Layout>
+</template>
+
+<script>
+    import Apps from '../layouts/Apps'
+    import Categories from '../layouts/Categories'
+
+
+    export default {
+        metaInfo: {
+            title: 'Apps'
+        },
+        components: {
+            Apps, Categories
+        }
+    }
+</script>
+
+<style scoped>
+    .container-inner {
+        margin: auto;
+        display: contents;
+        width: auto;
+    }
+
+    .flex-grow {
+        /*margin-left: 124px;*/
+        width: 70%;
+    }
+
+    .list {
+        width: 256px;
+        /*max-width: 100%;*/
+    }
+
+    .apps {
+        float: left;
+        margin: 64px;
+        padding: 20px;
+        margin-top: 64px;
+        flex-flow: wrap;
+        display: inline-box;
+    }
+</style>
