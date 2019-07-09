d5df342e9dd0fcf0d3aca455552ad3e0a350fcb0
Shreyas Nagare <shreyasnagare@gmail.com>
2019-07-09 18:58:31 -0400

    Fix categories list: position and relative size
    Release note: None

--- src/pages/Apps.vue
+++ src/pages/Apps.vue
@@ -5,10 +5,10 @@
             <h1 class="text-4xl font-bold leading-tight m-8">Apps</h1>
 
             <div class="w-full sm:flex text-xl text-gray-600 mx-auto">
-                <div class="l-auto h-auto md:w-1/6 h-12 bg-gray-200 rounded-lg  px-4 py-2 m-8">
+                <div class="l-auto h-auto h-12 bg-gray-200 rounded-lg  px-4 py-2 m-8">
                     <categories class="list"/>
                 </div>
-                <div class="w-full h-auto md:w-5/6 h-12 overflow-auto bg-gray-100 rounded-lg px-auto py-2 md:m-8">
+                <div class="w-full h-auto h-12 overflow-auto bg-gray-100 rounded-lg px-auto py-2 md:m-8">
                     <apps/>
                 </div>
             </div>
