a449169bc8240965a8af0e50f1234fbd8b5f9434
Shreyas Nagare <shreyasnagare@gmail.com>
2019-07-12 14:04:33 -0400

    Changed directory name for tools
    Release note: None

--- gridsome.config.js
+++ gridsome.config.js
@@ -40,7 +40,7 @@ module.exports = {
     {
       use: '@gridsome/source-filesystem',
       options: {
-        path: 'apps/**/*.md',
+        path: 'tools/**/*.md',
         typeName: 'App',
         refs: {
           categories: {
