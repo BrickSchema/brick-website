964a2945b17691259b9a5d13d7d6bef4a18c4e93
Shreyas Nagare <shreyasnagare@gmail.com>
2019-07-16 14:01:04 -0400

    Remove webpage title heading from the page
    Release note: None

--- src/templates/Webpage.vue
+++ src/templates/Webpage.vue
@@ -1,7 +1,6 @@
 <template>
     <Layout>
         <div class="container-inner mx-auto my-16">
-            <h1 class="text-4xl font-bold leading-tight">{{ $page.webpage.title }}</h1>
             <div class="markdown-body my-16 " v-html="$page.webpage.content" />
         </div>
     </Layout>
