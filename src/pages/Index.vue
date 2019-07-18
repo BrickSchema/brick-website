39cb975d8b8b4a572b9fa91da71a4c1b9a652ee8
Shreyas Nagare <shreyasnagare@gmail.com>
2019-07-18 19:44:17 -0400

    Fix homepage image, title and subtitle
    Release note: None

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,8 +2,8 @@
   <Layout>
 
     <div class="hero container-inner mx-auto flex flex-col sm:flex-row justify-between py-16">
-      <div class="w-1/4 mt-8 sm:mt-0">
-        <g-image src="../../static/logo.png" alt="hero" class="mx-auto sm:mx-0"/>
+      <div class="w-1/6 ml-32 sm:mt-0">
+        <g-image src="../../static/logo.png" alt="hero" class="h-auto mx-auto sm:mx-0"/>
       </div>
       <div class="text-4xl font-bold w-full sm:w-3/5 text-center sm:text-left">
         <div class="leading-tight">Brick</div>
