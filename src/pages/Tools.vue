b5eae6035efd6c9a0bd17ff0b3384dfdc9d004b1
Shreyas Nagare <shreyasnagare@users.noreply.github.com>
2019-07-18 16:29:21 -0400

    Revert "Horizontal tool-cards"

--- src/pages/Tools.vue
+++ src/pages/Tools.vue
@@ -1,15 +1,15 @@
 <template>
     <Layout>
-        <div class="container-inner mx-auto py-16">
+        <div class="mx-auto py-16 w-5/6">
 
-            <h1 class="text-4xl ml-4 pb-2 font-bold leading-tight border-b border-primary-200">Tools</h1>
+            <h1 class="text-4xl font-bold leading-tight m-8">Tools</h1>
 
-            <div class="w-full sm:flex text-xl text-gray-600 sm:mx-auto">
-                <div class="h-auto border-r border-primary-200">
+            <div class="w-full sm:flex text-xl text-gray-600 mx-auto">
+                <div class="l-auto h-auto h-12 bg-gray-200 rounded-lg  px-4 py-2 m-8">
                     <categories class="list"/>
                 </div>
-                <div class="w-full px-auto p-4">
-                    <apps />
+                <div class="w-full h-auto h-12 overflow-auto bg-gray-100 rounded-lg px-auto py-2 md:m-8">
+                    <apps/>
                 </div>
             </div>
 
