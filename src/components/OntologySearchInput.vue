92e88fb16ebaaeeb5912ec9eea62069f95c8ca32
Shreyas Nagare <shreyasnagare@gmail.com>
2019-07-29 17:28:28 -0400

    Search results links don't work with pathPrefix
    Fixes #42
    
    Release note: None

--- src/components/OntologySearchInput.vue
+++ src/components/OntologySearchInput.vue
@@ -36,7 +36,7 @@
           <a
                   v-for="(post, index) in results"
                   :key="index"
-                  :href="post.item.path"
+                  :href="$static.metaData.pathPrefix + post.item.path"
                   @click="reset"
                   class="bg-background-form border-b border-gray-400 text-xl cursor-pointer p-4 search-hover text-gray-700"
                   :class="{ 'search-highlighted' : index === highlightedIndex }"
