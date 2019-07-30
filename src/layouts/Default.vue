597f2c8c7c3ec07fe6e8c3d1df857004ae17d9d7
Shreyas Nagare <shreyasnagare@gmail.com>
2019-07-30 16:30:10 -0400

    Fixes outlines on mobile devices
    For the hamburger menu on default layout.
    
    Release note: None

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -15,7 +15,7 @@
                     </g-link>
                 </div>
                 <div class="block lg:hidden">
-                    <button @click="toggle" class="flex items-center px-3 py-2 border rounded border-gray-500 hover:text-gray-600 hover:border-gray-600">
+                    <button @click="toggle" class="focus:outline-none flex items-center px-3 py-2 border rounded border-gray-500 hover:text-gray-600 hover:border-gray-600">
                         <svg class="current-color h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" fill="gray" /></svg>
                     </button>
                 </div>
