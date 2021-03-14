4a91557baea13d4525f9434faef0d652b196dde0
shreyasnagare <shreyasnagare@gmail.com>
2021-03-14 18:15:57 -0400

    Add v1.2 ontology page

--- src/pages/Ontology.vue
+++ src/pages/Ontology.vue
@@ -38,6 +38,9 @@
                     <div class="text-3xl font-bold mb-8 w-full border-b-2 pb-4">Versions</div>
                     <div class="text-2xl font-bold">
                         <li class="mb-6">
+                            <g-link to="/ontology/1.2">v1.2</g-link>
+                        </li>
+                        <li class="mb-6">
                             <g-link to="/ontology/1.1">v1.1</g-link>
                         </li>
                         <li>
